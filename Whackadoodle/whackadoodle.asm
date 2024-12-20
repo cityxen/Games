d09033604386ecf9c459d3e7acdf5772ded61252
deadline-cxn <deadline@cityxen.net>
2024-12-20 11:17:47 -0500

    comment updates

--- Whackadoodle/whackadoodle.asm
+++ Whackadoodle/whackadoodle.asm
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////
 // WHACKADOODLE for C64 by Deadline / CityXen 2024
 //
-// Meatloaf support by Jaime Idolpx (Meatloaf guy)
+// Cartridge code & Meatloaf support by Jaime Idolpx (Meatloaf guy)
 //
 // Fairground tune by Saul Cross
 //
